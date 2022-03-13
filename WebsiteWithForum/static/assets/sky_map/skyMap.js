6f65dc317fbc86881bb894a2db12bd22701618c3
danchosocute <79661937+danchosocute@users.noreply.github.com>
2022-03-13 02:11:39 +0200

    Add files via upload

---
+++ WebsiteWithForum/static/assets/sky_map/skyMap.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+
+    var video = document.getElementById("video");
+    var playButton = document.getElementById("play-pause");
+    var fullScreenButton = document.getElementById("full-screen");
+    var seekBar = document.getElementById("seek-bar");
+    var today = new Date();
+    if(today.getHours() > 12) {
+        var time = (today.getHours() - 12 + today.getMinutes()/60) * 7.14;
+    } else {
+        var time = (today.getHours() + 12 + today.getMinutes()/60) * 7.14;
+    }
+
+
+    document.getElementById('video').addEventListener('loadedmetadata', function() {
+        this.currentTime = time;
+    },  false);
+
+    playButton.addEventListener("click", function() {
+        if (video.paused == true) {
+
+            video.play();
+            playButton.innerHTML = "Pause";
+        } else {
+
+            video.pause();
+            playButton.innerHTML = "Play";
+        }
+    });
+
+    fullScreenButton.addEventListener("click", function() {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        }
+    });
+
+    seekBar.addEventListener("input", function() {
+
+        var time = video.duration * (seekBar.value / 100);
+        video.currentTime = time;
+    });
+
+    video.addEventListener("timeupdate", function() {
+
+        var value = (100 / video.duration) * video.currentTime;
+        seekBar.value = value;
+    });
+
+    seekBar.addEventListener("mouseup", function() {
+        video.play();
+    });
+}
